10ceedb0be302ed104264ded9118cb5d60ab5111
Mojtaba Gharibreza <mojtabaqri@gmail.com>
2020-07-01 20:49:50 +0430

    add myinfo method

--- bot.php
+++ bot.php
@@ -3,6 +3,7 @@ require_once 'methods.php';
 require_once 'config.php';
 //end require
 $data=json_decode(file_get_contents("php://input"));
+file_put_contents('data.json',json_encode($data));
 //if message has attachment $attachment is true ---------------------
 if (array_key_exists('channel_post',$data)){
     $channelId=$data->channel_post->chat->id;
@@ -17,6 +18,21 @@ if (array_key_exists('channel_post',$data)){
     else
         exit();
 
+}else{
+    $name=$data->message->from->first_name.chr(10);;
+    $username="@".$data->message->from->username.chr(10);;
+    $id=$data->message->from->id;
+    $text="مشخصات شما : 
+✅نام :  $name
+✅یوزرنیم : $username
+✅چت آیدی : $id
+\n
+";
+    if($data->message->text=="myinfo")
+        SendMessage($id,$text);
+    else
+        exit();
 }
+
 
 
